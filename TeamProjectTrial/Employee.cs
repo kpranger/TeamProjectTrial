4160fa2ef800c0a017f0a609215c1da18ebadee8
Kris <pranger.kris@gmail.com>
2022-07-07 14:32:21 -0400

    Added employee class

---
+++ TeamProjectTrial/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProjectTrial
+{
+    internal class Employee
+    {
+        //created properties
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public string Position { get; set; }
+        //created constructor
+        public Employee(string _name, double _salary, string _position)
+        {
+            Name = _name;
+            Salary = _salary;
+            Position = _position;
+        }
+    }
+}
