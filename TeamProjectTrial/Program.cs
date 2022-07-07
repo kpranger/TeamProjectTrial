f1bb9101d2c468a9be1c4df91e50906090d32cab
JaraMohnson <jaramohnson@gmail.com>
2022-07-07 14:39:18 -0400

    added employee names/positions

--- TeamProjectTrial/Program.cs
+++ TeamProjectTrial/Program.cs
@@ -1,1 +1,12 @@
 ﻿
+
+using TeamProjectTrial;
+
+Employee Mara = new Employee("Mara", 80000, "Manager");
+
+Employee Kris = new Employee("Kris", 65000, "Team Chef");
+
+Employee Krista = new Employee("Krista", 80000, "Senior Dev");
+
+Employee Brandon = new Employee("Brandon", 79000, "Office Pet Supervisor");
+
